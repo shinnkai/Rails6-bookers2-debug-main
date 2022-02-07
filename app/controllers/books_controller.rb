8fa97ca7ad825cc975203c0bcdcb660710c85315
EC2 Default User <ec2-user@ip-172-31-17-169.ec2.internal>
2022-02-07 08:12:13 +0000

    Revert "Rails6-bookers2-debug-main-応用課題3"
    This reverts commit 6ebd28cca9e9edac00ada8d2d2e7e6078c5be9b0.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,6 @@ class BooksController < ApplicationController
     @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
-    @book_comment = BookComment.new
   end
 
   def index
